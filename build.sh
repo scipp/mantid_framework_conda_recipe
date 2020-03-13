c72a9c16aa03f9d5f6873f71ec122761e142d86d
Dan Nixon <dan@dan-nixon.com>
2020-03-13 15:07:22 +0000

    Mantid framework Conda recipe
    Builds and installs, can load data with Scipp.

---
+++ build.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+set -ex
+
+mkdir build
+cd build
+
+cmake \
+  -G Ninja \
+  -DUSE_SYSTEM_EIGEN=ON \
+  -DENABLE_OPENGL=OFF \
+  -DENABLE_MANTIDPLOT=FALSE \
+  -DENABLE_WORKBENCH=FALSE \
+  -DENABLE_OPENCASCADE=FALSE \
+  -DPYTHON_EXECUTABLE="$CONDA_PREFIX/bin/python" \
+  -DCMAKE_PREFIX_PATH=$CONDA_PREFIX \
+  -DCMAKE_INSTALL_PREFIX=$CONDA_PREFIX \
+  -DCMAKE_SKIP_INSTALL_RPATH=ON \
+  -DBOOST_INCLUDEDIR="$CONDA_PREFIX/include" \
+  ..
+
+cmake --build .
+cmake --build . --target install
+
+# Move Mantid
+python_site_pkg_path="$(python -c 'from __future__ import print_function; import h5py, os; opd=os.path.dirname; print(opd(opd(h5py.__file__)))')"
+echo "$python_site_pkg_path"
+
+mv "$CONDA_PREFIX/lib/mantid/" "$python_site_pkg_path/"
+mv "$CONDA_PREFIX/lib/"mantid-*-py*.egg-info "$python_site_pkg_path/"
+# ln -s "$CONDA_PREFIX/plugins" "$CONDA_PREFIX/lib/mantid/plugins"
