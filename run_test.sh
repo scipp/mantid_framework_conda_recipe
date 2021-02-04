9477ab30749ee4dabac95e6dbdc5b85a82e7841b
Owen Arnold <owen.arnold@stfc.ac.uk>
2021-02-04 15:25:46 +0000

    Use compat tests in conda build

---
+++ run_test.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -e
+set +x
+
+python -c"import mantid.kernel"
+python -c"import mantid.geometry"
+python -c"import mantid.api"
+
+conda install -c scipp -c conda-forge scipp psutil pytest git --yes
+
+python -c"import mantid;print('Compat testing using mantid', mantid.__version__)"
+python -c"import scipp;print('Compat testing using scipp', scipp.__version__)"
+
+git clone https://github.com/scipp/scipp.git
+cd scipp
+git fetch --all --tags
+git checkout tags/0.0.5 
+python -m pytest python/tests/compat/test_mantid.py 
