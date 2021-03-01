11ffb914027cde60f34154140016cc61947a4556
Owen Arnold <owen.arnold@stfc.ac.uk>
2021-03-01 13:18:09 +0000

    Correct run_tests.sh
    Quite a few small errors fixed

--- run_test.sh
+++ run_test.sh
@@ -6,13 +6,13 @@ python -c"import mantid.kernel"
 python -c"import mantid.geometry"
 python -c"import mantid.api"
 
-conda install -c scipp -c conda-forge scipp psutil pytest git --yes
+conda install -c scipp/label/dev -c conda-forge scippneutron psutil pytest git matplotlib-base=3.2.2 pythreejs --yes
 
 python -c"import mantid;print('Compat testing using mantid', mantid.__version__)"
 python -c"import scipp;print('Compat testing using scipp', scipp.__version__)"
 
-git clone https://github.com/scippneutron/scipp.git
-cd scipp
+git clone https://github.com/scipp/scippneutron.git scippneutron
+cd scippneutron
 git fetch --all --tags
-git checkout cross-dr-testing
+git checkout more-mantid-scipp-comparisons
 python -m pytest python/tests/ 
