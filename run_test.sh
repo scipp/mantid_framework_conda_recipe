8c470fe846a66693aa2ea751e404229f9a724569
Owen Arnold <owen.arnold@stfc.ac.uk>
2021-03-17 11:40:25 +0000

    Unpin matplotlib-base
    matplotlib-base has boost-cpp dependency. Pinning earlier version caused frozen version (1.74 in this case) to require downgrading for boost (previously 1.75).

--- run_test.sh
+++ run_test.sh
@@ -6,7 +6,7 @@ python -c"import mantid.kernel"
 python -c"import mantid.geometry"
 python -c"import mantid.api"
 
-conda install -c scipp/label/dev -c conda-forge scippneutron psutil pytest git matplotlib-base=3.2.2 pythreejs --yes
+conda install -c scipp/label/dev -c conda-forge scippneutron psutil pytest git matplotlib-base pythreejs --yes
 
 python -c"import mantid;print('Compat testing using mantid', mantid.__version__)"
 python -c"import scippneutron;print('Compat testing using scippneutron', scippneutron.__version__)"
